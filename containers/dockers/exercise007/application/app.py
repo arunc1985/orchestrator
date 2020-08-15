9bfe377cc724a4aa859f9ebfdfc8feab0895cac6
Arun Kumar Chandramouli -X (arunkuch - HCL TECHNOLOGIES LIMITED at Cisco) <arunkuch@cisco.com>
2020-08-15 16:20:44 +0530

    Testing

--- containers/dockers/exercise007/application/app.py
+++ containers/dockers/exercise007/application/app.py
@@ -1,25 +1,12 @@
-import pymongo
+from flask import Flask
+import os,sys
 
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-mydb = myclient["mydatabase"]
-mycol = mydb["customers"]
+app = Flask(__name__)
 
-mylist = [
-  { "name": "Amy", "address": "Apple st 652"},
-  { "name": "Hannah", "address": "Mountain 21"},
-  { "name": "Michael", "address": "Valley 345"},
-  { "name": "Sandy", "address": "Ocean blvd 2"},
-  { "name": "Betty", "address": "Green Grass 1"},
-  { "name": "Richard", "address": "Sky st 331"},
-  { "name": "Susan", "address": "One way 98"},
-  { "name": "Vicky", "address": "Yellow Garden 2"},
-  { "name": "Ben", "address": "Park Lane 38"},
-  { "name": "William", "address": "Central st 954"},
-  { "name": "Chuck", "address": "Main Road 989"},
-  { "name": "Viola", "address": "Sideway 1633"}
-]
+@app.route('/welcome/<fname>/<lname>')
+def hello_world(fname,lname):
+  return {True:"Hello There! {} {}!! How are you !  Welcome to Docker!".format(fname,lname)}
 
-x = mycol.insert_many(mylist)
-
-#print list of the _id values of the inserted documents:
-print(x.inserted_ids)
+if __name__ == "__main__":
+  print(os.environ['CODER'])
+  app.run(host=os.environ['FLASKHOSTNAME'],port=os.environ['FLASKPORT'],debug=True)
