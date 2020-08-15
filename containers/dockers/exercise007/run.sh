221339e8e0763a097707695722208534c77a0dea
Arun Kumar Chandramouli -X (arunkuch - HCL TECHNOLOGIES LIMITED at Cisco) <arunkuch@cisco.com>
2020-08-15 17:13:24 +0530

    Add code for collection drop

--- containers/dockers/exercise007/run.sh
+++ containers/dockers/exercise007/run.sh
@@ -9,6 +9,7 @@ docker rm -f flaskpy3
 docker run -d -v /root/orchestrator/containers/dockers/exercise007/application/:/tests --name flaskpy3 -e FLASKHOSTNAME='0.0.0.0' -e FLASKPORT=5500 -e CODER='By Arun!' -p 5500:5500 volumes-tests-flask:v1
 
 
+echo "Do a Curl and test data"
 curl -XGET http://localhost:5500/welcome/Arun/Chandramouli
 
 
