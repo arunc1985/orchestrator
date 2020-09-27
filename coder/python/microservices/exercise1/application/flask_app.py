7f44e2764a68e58203122d795aa35eb470eebb74
Arun Kumar Chandramouli -X (arunkuch - HCL TECHNOLOGIES LIMITED at Cisco) <arunkuch@cisco.com>
2020-09-27 11:04:22 +0530

    Flask Basics

---
+++ coder/python/microservices/exercise1/application/flask_app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import os,sys
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+  return {True:"Hello There! Welcome to K8s!"}
+
+@app.route('/welcome/<fname>/<lname>')
+def hello_world(fname,lname):
+  return {True:"Hello There! {} {}!! How are you !  Welcome to Docker!".format(fname,lname)}
+
+if __name__ == "__main__":
+    app.run(host="localhost",port=5000,debug=True)
