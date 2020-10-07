121788e8f45d9d9ea34ca73a7989812643103e7d
Arun Kumar Chandramouli -X (arunkuch - HCL TECHNOLOGIES LIMITED at Cisco) <arunkuch@cisco.com>
2020-10-07 19:02:07 +0530

    Add k8s yaml file and update README

--- containers/dockers/exercise004/application/flask_app.py
+++ containers/dockers/exercise004/application/flask_app.py
@@ -7,6 +7,11 @@ app = Flask(__name__)
 def hello():
   return {True:"I Love Docker and K8s"}
 
+@app.route('/dockers')
+def dockers():
+  return {True:"Dockers and K8s"}
+
+
 @app.route('/welcome/<fname>/<lname>')
 def hello_world(fname,lname):
   return {True:"Hello There! {} {}!! How are you !  Welcome to Docker!".format(fname,lname)}
